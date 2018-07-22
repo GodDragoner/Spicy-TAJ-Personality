fef2bab86aa07ea4a4dcfe9a8f3c9a0c47bfbf0a
lotar232 <36368222+lotar232@users.noreply.github.com>
2018-07-21 22:49:44 -0700

    Add files via upload
    implemented house rules and reporting exercise

---
+++ rules/HouseRules.js
@@ -0,0 +1,14 @@
+sendVirtualAssistantMessage("These are the house rules slave.");
+sendVirtualAssistantMessage("Make sure you know these by heart!");		
+
+showImage("Images/Spicy/HouseRules/HouseRule1.jpg",10);
+showImage("Images/Spicy/HouseRules/HouseRule2.jpg",10);
+showImage("Images/Spicy/HouseRules/HouseRule3.jpg", 10);
+showImage("Images/Spicy/HouseRules/HouseRule4.jpg", 10);
+showImage("Images/Spicy/HouseRules/HouseRule5.jpg", 10);
+showImage("Images/Spicy/HouseRules/HouseRule6.jpg", 10);
+showImage("Images/Spicy/HouseRules/HouseRule7.jpg", 10);
+showImage("Images/Spicy/HouseRules/HouseRule8.jpg", 10);
+showImage("Images/Spicy/HouseRules/HouseRule9.jpg", 10);
+showImage("Images/Spicy/HouseRules/HouseRule10.jpg", 10);
+
