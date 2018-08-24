e058278035cef948bd910f367dfb91b1bd83b0af
lotar232 <36368222+lotar232@users.noreply.github.com>
2018-08-23 20:44:36 -0700

    Add files via upload
    "rules" and "Exercise" pretty functional... on to dungeon next

---
+++ Exercise/ExerciseLevel.js
@@ -0,0 +1,38 @@
+
+//  this file adjusts the exercise routine to be more easy/difficult based on recent performance
+if (getVar("ExerciseLevelMastered") > 4)
+	{//uplevel
+	setVar("ExerciseLevel", getVar("ExerciseLevel")+1); 
+	if (getVar("ExerciseLevel") > 10)
+		{setVar("ExerciseLevel", 10);
+		}
+	setVar("ExerciseLevelMastered",0);
+	setVar("ExerciseLevelFailed",0);
+	
+	sendVirtualAssistantMessage("%SlaveName%, I've noticed you've been doing really well in your workouts lately ");
+	sendVirtualAssistantMessage(random("That makes me wet"," That gets me excited", "It looks like its beginning to pay off") );
+	sendVirtualAssistantMessage(random("Its time to move you to the next level"," I think you can step it up a notch for %DomHonorific% %domName%"," I want to see if you can take a little more for %domName%"), "%Smile%");
+
+	}
+	else{if (getVar("ExerciseLevelFailed") > 4)
+		{//downlevel
+		setVar("ExerciseLevel", getVar("ExerciseLevel")-1); 
+		if (getVar("ExerciseLevel") < 0)
+			{setVar("ExerciseLevel", 0);
+			}
+
+		setVar("ExerciseLevelMastered",0);
+		setVar("ExerciseLevelFailed",0);
+
+		setVar("punishmentpoints", getVar("punishmentpoints")+100); 
+		setVar("BadExerciseEffort", true);
+
+		sendVirtualAssistantMessage("%SlaveName%, we need to talk about something...");
+		sendVirtualAssistantMessage("%DomHonorific% %DomName% and I have noticed you're not able to keep up with the workouts you're assigned.");
+		sendVirtualAssistantMessage(random("That's disappointing","I'm a little embarrassed for you","Your %DomHonorific% deserves better"));
+		sendVirtualAssistantMessage(random("I care about you","%DomHonorific% %DomName% wants her %Slave% to succeed","We want to make sure you make progress") + " So I'm dropping you down a level.");
+		sendVirtualAssistantMessage("Of course," , random("to maintain discipline"," to keep you incentivized"," to provide motivation")  + " I'm assigning you a special Punishment.");
+		sendVirtualAssistantMessage(random("My decision on this is final","Try not to disappoint %DomHonorific% %DomName%","You're welcome") + " %SlaveName%");
+		
+		}
+	}
