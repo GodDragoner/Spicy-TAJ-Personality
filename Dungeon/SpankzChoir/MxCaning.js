174b78f618fde371cdec4899464a60133f47498d
lotar2323 <46235335+lotar2323@users.noreply.github.com>
2019-06-16 19:20:53 -0700

    uploading spankzchoir js files
    haven't used since feb/march but recall this was working (mostly?) code at the time...

---
+++ Dungeon/SpankzChoir/MxCaning.js
@@ -0,0 +1,68 @@
+
+ SpankingCount=0;
+MXmessage( random("Get the rubberbands around your asscheek","Get the rubberbands ready","Prepare the rubberbands..") );
+ sleep(12);
+MXlapmessage( random("Get over my lap and remember to count out loud..","Get over my lap","Get over my lap","Prepare yourself and get over my lap","Over my lap")+" %SlaveName%!"); 
+sleep(12);
+//(Spanking)
+while (SpankingCount<getVar("StrokesPerRound"))
+	{	
+		 strokes=random(10,20,30);
+		 switch(strokes)
+		 {
+
+			case 10:
+			SpankingCount=SpankingCount+20;
+			MXoverlapmessage( random("Left","Top left","Buttom left")+ " " + random("ass","asscheek","Buttcheek","cheek","rear cheek","rear","sitspots!")); 
+				 playAudio("Audio/GNMSounds/Punishment/SpankingTask/10Strikes/Spank10.mp3", true);
+
+			MXoverlapmessage( random("Right","Top Right","Buttom Right") +" "+ random("ass","asscheek","Buttcheek","cheek","rear cheek","rear","sitspots!")) ;
+			 playAudio("Audio/GNMSounds/Punishment/SpankingTask/10Strikes/Spank10.mp3", true);
+			 break;
+			case 20:
+			SpankingCount=SpankingCount+40;
+			MXoverlapmessage( random("Left","Top left","Buttom left") +" "+ random("ass","asscheek","Buttcheek","cheek","rear cheek","rear","sitspots!")); 
+				 playAudio("Audio/GNMSounds/Punishment/SpankingTask/20Strikes/Spank20.mp3", true);
+
+			MXoverlapmessage( random("Right","Top Right","Buttom Right") +" "+ random("ass","asscheek","Buttcheek","cheek","rear cheek","rear","sitspots!"));
+			 playAudio("Audio/GNMSounds/Punishment/SpankingTask/20Strikes/Spank20.mp3", true);
+			 break;
+				case 30:
+			SpankingCount=SpankingCount+60;
+			MXoverlapmessage( random("Left","Top left","Buttom left")+" "+ random("ass","asscheek","Buttcheek","cheek","rear cheek","rear","sitspots!")); 
+				 playAudio("Audio/GNMSounds/Punishment/SpankingTask/30Strikes/Spank30.mp3", true);
+
+			MXoverlapmessage( random("Right","Top Right","Buttom Right") +" "+ random("ass","asscheek","Buttcheek","cheek","rear cheek","rear","sitspots!"));
+			 playAudio("Audio/GNMSounds/Punishment/SpankingTask/30Strikes/Spank30.mp3", true);
+			 break;
+
+		 }
+	}
+//(End1)
+Guess= sendInput(" How many strokes were that slave? ");
+
+if (guess.getInt() == SpankingCount)
+	{MXmessage(" Correct! ");
+	 setVar("TotalSpanking",getVar("TotalSpanking") + SpankingCount);
+	}
+	else
+	{
+	MXmessage(" Wrong! ");
+	MXmessage(" The number was "+SpankingCount );
+	 changeMeritMedium(true);
+	 chance= random(1,5);
+	 if (chance>2)
+	 {
+		MXmessage(" Since you've failed to count I guess we're starting over! "); 
+		run("Dungeon/SpankzChoir/MXSpanking.js");
+	 }
+	 else
+	 {
+	
+		MXmessage(" I'm gonna be merciful this time ");
+		setVar("TotalSpanking",getVar("TotalSpanking") + SpankingCount);
+		MXmessage(" Don't expect me to show kindness again! ");
+	 }
+
+	}
+	
---
+++ Dungeon/SpankzChoir/MxSpanking.js
@@ -0,0 +1,201 @@
+//(Reset)
+//@NullResponse @SetVar[SpankingCount]=[0]
+setVar("SpankingCount",0);
+
+MXmessage(" %Fetch% %ChooseSpankingImplement% ", 12); 
+MXlapmessage(random("Get over my lap and remember to count out loud..","Get over my lap","Get over my lap","Prepare yourself and get over my lap","Over my lap") + " %SlaveName%! ", 12);
+//(Spanking)
+
+while ( getVar("SpankingCount") < getVar("StrokesPerRound"))
+{
+spanks=randomInteger(1,5);
+
+spanks=spanks*10;
+switch(spanks)
+{
+	case 10:
+	fastorslow=randomInteger(0,1);
+	 setVar("SpankingCount",getVar("SpankingCount")+10 );
+	 	MXoverlapmessage(random("left","left","bottom left","top left")+" " + random("ass!","asscheek!","buttcheek!","cheek!","rear cheek","rear","sitspots!") ); 
+	 if (fastorslow==0)
+		{ playAudio("Audio/GNMSounds/Punishment/SpankingTask/10Strikes/Spank10.mp3", true);
+		 sleep(17);
+		}
+		else
+		{ MXoverlapmessage(random("Fast ones!","Quick ones!","Speedy ones!"));
+		 playAudio("Audio/GNMSounds/Punishment/SpankingTask/10Strikes/Spank10Fast.mp3", true);
+		sleep(9);
+		}
+		
+	fastorslow=randomInteger(0,1);
+	 setVar("SpankingCount",getVar("SpankingCount")+10 );
+	 	MXoverlapmessage(random("right","right","bottom right","top right")+" " + random("ass!","asscheek!","buttcheek!","cheek!","rear cheek","rear","sitspots!") ); 
+	 if (fastorslow==0)
+		{ playAudio("Audio/GNMSounds/Punishment/SpankingTask/10Strikes/Spank10.mp3", true);
+		 sleep(17);
+		}
+		else
+		{ MXoverlapmessage(random("Fast ones!","Quick ones!","Speedy ones!"));
+		 playAudio("Audio/GNMSounds/Punishment/SpankingTask/10Strikes/Spank10Fast.mp3", true);
+		sleep(9);
+		}
+		
+	break;
+	
+	
+		case 20:
+	fastorslow=randomInteger(0,1);
+	 setVar("SpankingCount",getVar("SpankingCount")+20 );
+	 	MXoverlapmessage(random("left","left","bottom left","top left")+" " + random("ass!","asscheek!","buttcheek!","cheek!","rear cheek","rear","sitspots!") ); 
+	 if (fastorslow==0)
+		{ playAudio("Audio/GNMSounds/Punishment/SpankingTask/20Strikes/Spank20.mp3", true);
+		 sleep(34);
+		}
+		else
+		{ MXoverlapmessage(random("Fast ones!","Quick ones!","Speedy ones!"));
+		 playAudio("Audio/GNMSounds/Punishment/SpankingTask/20Strikes/Spank20Fast.mp3", true);
+		sleep(17);
+		}
+		
+	fastorslow=randomInteger(0,1);
+	 setVar("SpankingCount",getVar("SpankingCount")+20 );
+	 	MXoverlapmessage(random("right","right","bottom right","top right")+" " + random("ass!","asscheek!","buttcheek!","cheek!","rear cheek","rear","sitspots!") ); 
+	 if (fastorslow==0)
+		{ playAudio("Audio/GNMSounds/Punishment/SpankingTask/20Strikes/Spank20.mp3", true);
+		 sleep(34);
+		}
+		else
+		{ MXoverlapmessage(random("Fast ones!","Quick ones!","Speedy ones!"));
+		 playAudio("Audio/GNMSounds/Punishment/SpankingTask/20Strikes/Spank20Fast.mp3", true);
+		sleep(17);
+		}
+		
+	break;
+	
+	
+		case 30:
+	fastorslow=randomInteger(0,1);
+	 setVar("SpankingCount",getVar("SpankingCount")+30 );
+	 	MXoverlapmessage(random("left","left","bottom left","top left")+" " + random("ass!","asscheek!","buttcheek!","cheek!","rear cheek","rear","sitspots!") ); 
+	 if (fastorslow==0)
+		{ playAudio("Audio/GNMSounds/Punishment/SpankingTask/30Strikes/Spank30.mp3", true);
+		 sleep(50);
+		}
+		else
+		{ MXoverlapmessage(random("Fast ones!","Quick ones!","Speedy ones!"));
+		 playAudio("Audio/GNMSounds/Punishment/SpankingTask/30Strikes/Spank30Fast.mp3", true);
+		sleep(25);
+		}
+		
+	fastorslow=randomInteger(0,1);
+	 setVar("SpankingCount",getVar("SpankingCount")+30 );
+	 	MXoverlapmessage(random("right","right","bottom right","top right")+" " + random("ass!","asscheek!","buttcheek!","cheek!","rear cheek","rear","sitspots!") ); 
+	 if (fastorslow==0)
+		{ playAudio("Audio/GNMSounds/Punishment/SpankingTask/30Strikes/Spank30.mp3", true);
+		 sleep(50);
+		}
+		else
+		{ MXoverlapmessage(random("Fast ones!","Quick ones!","Speedy ones!"));
+		 playAudio("Audio/GNMSounds/Punishment/SpankingTask/30Strikes/Spank30Fast.mp3", true);
+		sleep(25);
+		}
+		
+	break;
+	
+	
+		case 40:
+	fastorslow=randomInteger(0,1);
+	 setVar("SpankingCount",getVar("SpankingCount")+40 );
+	 	MXoverlapmessage(random("left","left","bottom left","top left")+" " + random("ass!","asscheek!","buttcheek!","cheek!","rear cheek","rear","sitspots!") ); 
+	 if (fastorslow==0)
+		{ playAudio("Audio/GNMSounds/Punishment/SpankingTask/40Strikes/Spank40.mp3", true);
+		 sleep(67);
+		}
+		else
+		{ MXoverlapmessage(random("Fast ones!","Quick ones!","Speedy ones!"));
+		 playAudio("Audio/GNMSounds/Punishment/SpankingTask/40Strikes/Spank40Fast.mp3", true);
+		sleep(33);
+		}
+		
+	fastorslow=randomInteger(0,1);
+	 setVar("SpankingCount",getVar("SpankingCount")+40 );
+	 	MXoverlapmessage(random("right","right","bottom right","top right")+" " + random("ass!","asscheek!","buttcheek!","cheek!","rear cheek","rear","sitspots!") ); 
+	 if (fastorslow==0)
+		{ playAudio("Audio/GNMSounds/Punishment/SpankingTask/40Strikes/Spank40.mp3", true);
+		 sleep(67);
+		}
+		else
+		{ MXoverlapmessage(random("Fast ones!","Quick ones!","Speedy ones!"));
+		 playAudio("Audio/GNMSounds/Punishment/SpankingTask/40Strikes/Spank40Fast.mp3", true);
+		sleep(33);
+		}
+		
+	break;
+	
+	
+		case 50:
+	fastorslow=randomInteger(0,1);
+	 setVar("SpankingCount",getVar("SpankingCount")+50 );
+	 	MXoverlapmessage(random("left","left","bottom left","top left")+" " + random("ass!","asscheek!","buttcheek!","cheek!","rear cheek","rear","sitspots!") ); 
+	 if (fastorslow==0)
+		{ playAudio("Audio/GNMSounds/Punishment/SpankingTask/50Strikes/Spank50.mp3", true);
+		 sleep(83);
+		}
+		else
+		{ MXoverlapmessage(random("Fast ones!","Quick ones!","Speedy ones!"));
+		 playAudio("Audio/GNMSounds/Punishment/SpankingTask/50Strikes/Spank50Fast.mp3", true);
+		sleep(40);
+		}
+		
+	fastorslow=randomInteger(0,1);
+	 setVar("SpankingCount",getVar("SpankingCount")+50 );
+	 	MXoverlapmessage(random("right","right","bottom right","top right")+" " + random("ass!","asscheek!","buttcheek!","cheek!","rear cheek","rear","sitspots!") ); 
+	 if (fastorslow==0)
+		{ playAudio("Audio/GNMSounds/Punishment/SpankingTask/50Strikes/Spank50.mp3", true);
+		 sleep(83);
+		}
+		else
+		{ MXoverlapmessage(random("Fast ones!","Quick ones!","Speedy ones!"));
+		 playAudio("Audio/GNMSounds/Punishment/SpankingTask/50Strikes/Spank50Fast.mp3", true);
+		sleep(40);
+		}
+		
+	break;
+	
+	
+	
+
+}
+}
+
+//(End1)
+
+guess= sendInput(" How many strokes were that slave? "); 
+if (guess.getInt() == getVar("SpankingCount"))
+	{MXmessage(" Correct! ");
+	 setVar("TotalSpanking",getVar("TotalSpanking") + getVar("SpankingCount"));
+	}
+	else
+	{
+	MXmessage(" Wrong! ");
+	MXmessage(" The number was "+getVar("SpankingCount") );
+	 changeMeritMedium(true);
+	 chance= random(1,5);
+	 if (chance>2)
+	 {
+		MXmessage(" Since you've failed to count I guess we're starting over! "); 
+		run("Dungeon/SpankzChoir/MXSpanking.js");
+	 }
+	 else
+	 {
+	
+		MXmessage(" I'm gonna be merciful this time ");
+		setVar("TotalSpanking",getVar("TotalSpanking") + getVar("SpankingCount"));
+		MXmessage(" Don't expect me to show kindness again! ");
+	 }
+
+	}
+	
+	
+	
+	
