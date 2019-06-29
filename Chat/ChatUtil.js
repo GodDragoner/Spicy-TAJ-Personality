b677e72bcdf0ab040207981abe1bf358946d0e8b
lotar2323 <46235335+lotar2323@users.noreply.github.com>
2019-06-29 08:41:14 -0700

    update

--- Chat/ChatUtil.js
+++ Chat/ChatUtil.js
@@ -46,37 +46,39 @@ function sendMessageBasedOnSender(message, secondsToWait = undefined, skipImage 
 }
 
 function sendVirtualAssistantMessage(message, wait, skipImage) {
-    let textName = new javafx.scene.text.Text("[Vivienne]: ");
+    textName = new javafx.scene.text.Text("[Vivienne]: ");
     textName.setFill(javafx.scene.paint.Color.ROYALBLUE);
     textName.setFont(javafx.scene.text.Font.font(null, javafx.scene.text.FontWeight.BOLD, 14));
 
     message = replaceVocab(message);
-    let text = new javafx.scene.text.Text(message);
+    text = new javafx.scene.text.Text(message);
     text.setFill(javafx.scene.paint.Color.ROYALBLUE);
     text.setFont(javafx.scene.text.Font.font(null, javafx.scene.text.FontWeight.MEDIUM, 13));
 
+//test below to see if tts is supported and sendmessage can be swapped for sendcustom message
     sendCustomMessage(textName, text);
 
     //Show image
     if(skipImage === undefined || !skipImage) {
-        if(!isImagesLocked()) {
+     //   if(!isImagesLocked()) {
             showImage("Images/Spicy/Assistant/" + ASSISTANT_CURRENT_SET_ID + "/*.jpg");
-        }
+      //  }
     }
 
-    if(wait === undefined || wait) {
+    if(wait === undefined ) {
         sleep(1000 + message.length * 50, "MILLISECONDS");
-    }
+    }else
+		{sleep(wait*1000, "MILLISECONDS");
+		}
 }
 
 function sendSystemMessage(message) {
-    let text = new javafx.scene.text.Text(message);
+    text = new javafx.scene.text.Text(message);
     text.setFill(javafx.scene.paint.Color.RED);
     text.setFont(javafx.scene.text.Font.font(null, javafx.scene.text.FontWeight.BOLD, 13));
 
     sendCustomMessage(text);
 }
-
 
 function addContact(id) {
     let contactName = "";
@@ -188,4 +190,30 @@ function createYesOrNoQuestion() {
             answer.loop();
         }
     }
+}
+
+function sendArbMessage(textName,message, wait, ImagePath) {
+ 
+    sendName = new javafx.scene.text.Text(textName);
+    sendName.setFill(javafx.scene.paint.Color.ROYALBLUE);
+    sendName.setFont(javafx.scene.text.Font.font(null, javafx.scene.text.FontWeight.BOLD, 14));
+
+
+    message = replaceVocab(message);
+    text = new javafx.scene.text.Text(message);
+    text.setFill(javafx.scene.paint.Color.ROYALBLUE);
+    text.setFont(javafx.scene.text.Font.font(null, javafx.scene.text.FontWeight.MEDIUM, 13));
+
+    sendCustomMessage(sendName, text);
+	    //Show image
+  //  if(ImagePath !== undefined) {
+  //      showImage(ImagePath);
+   // }
+
+    if(wait === undefined ) {
+        sleep(1000 + message.length * 50, "MILLISECONDS");
+    }else
+		{sleep(wait*1000, "MILLISECONDS");
+		}
+
 }
