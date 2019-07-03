b75a9400ea93261399ae3929cee666102677fa92
GodDragoner <goddragoner@gmail.com>
2019-07-03 21:10:57 +0200

    Working

--- dungeon/PunishmentBase.js
+++ dungeon/PunishmentBase.js
@@ -70,7 +70,7 @@ if (isVar("Punishment")) {
 }
 
 sendDungeonMessage("Yes %SlaveName%?");
-//let answer = createInput();
+let answer = createInput();
 
 while(true) {
     if (answer.containsIgnoreCase("how many", "point")) {
@@ -87,7 +87,7 @@ while(true) {
         } else if (punishmentPoints > 750) {
             sendDungeonMessage(random("Lol %SubName%, you're so fucked!", "Oh %SlaveName%, you're ass is gonna bleed", "You should be ashamed, %SlaveName%", "how did you let it get so bad?"));
         } else if (punishmentPoints > 500) {
-            sendDungeonMessage(random("this is SERIOUS %Slave%, you need to put some time in down here", "NOT GOOD, you need to put some serious time in down here", "I have a feeling we're going to be spending lots of time together %Grin%", "that's so many your %DomHonorific% won't even session with you"));
+            sendDungeonMessage(random("this is SERIOUS %Slave%, you need to put some time in down here", "NOT GOOD, you need to put some serious time in down here", "I have a feeling we're going to be spending lots of time together %GNMGrin%", "that's so many your %DomHonorific% won't even session with you"));
         } else if (punishmentPoints > 250) {
             sendDungeonMessage(random("we have some work to do down here", "I'll let the mistresses know they should plan to spend some time in the dungeon"));
         } else {
@@ -107,7 +107,7 @@ while(true) {
         //Delete any old variable that might still be in place
         delVar("Punishment");
 
-        sendDungeonMessage(random("Oh my", "Oh how nice", "Lovely!", "Splendid!", "Exciting!", "Fantastic", "%Grin%", "perfect...", "Good", "So you want to be punished."));
+        sendDungeonMessage(random("Oh my", "Oh how nice", "Lovely!", "Splendid!", "Exciting!", "Fantastic", "%GNMGrin%", "perfect...", "Good", "So you want to be punished."));
         sendDungeonMessage(random("Let me just look up your file...", "Checking your file", "Having a look at your file...", "Let me just check your file..."), 2);
         sendDungeonMessage("Take a seat...");
 
@@ -249,7 +249,7 @@ while(true) {
                     punishSeverity = 3;
                     break;
                 } else if (answer.containsIgnoreCase("Extreme")) {
-                    sendDungeonMessage("%Grin% A glutton for punishment...Setting it up...");
+                    sendDungeonMessage("%smile% A glutton for punishment...Setting it up...");
                     punishSeverity = 4;
                     break;
                 } else if (answer.isTimeout()) {
