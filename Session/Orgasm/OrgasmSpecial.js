aedce1ff4dcf41c3d52a758ef371cab6f19c8648
GodDragoner <goddragoner@gmail.com>
2020-01-11 03:54:56 +0100

    Syntax issues fixed

--- Session/Orgasm/OrgasmSpecial.js
+++ Session/Orgasm/OrgasmSpecial.js
@@ -6,7 +6,7 @@ function sendOrgasmChastityIntroduction() {
         sendMessage('Since this is the first time for you maybe cumming inside the chastity cage I am gonna explain the rules to you');
         sendMessage('You will have to stimulate yourself using the vibrator and maybe even edge');
         sendMessage('If I want you to ruin the orgasm you will slowly tip you over the edge and the completely remove the vibrator from the cage');
-        sendMessage('If I allow you to cum you will have to it within that small cage %Grin%');
+        sendMessage('If I allow you to cum you will have to do it within that small cage %Grin%');
         sendMessage('When allowed to cum you are allowed to stimulate yourself however you want');
         sendMessage('And well when I deny you...');
         sendMessage('You have to live with it %Grin%');
