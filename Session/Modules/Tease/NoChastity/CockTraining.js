918c0a8188b419bef6d9e291224d2691d4a4c98d
FrozenWolf <FrozenWolf4887@yahoo.com>
2021-02-17 15:18:37 +0000

    Tell user to stop stroking at level 50 of cock training

--- Session/Modules/Tease/NoChastity/CockTraining.js
+++ Session/Modules/Tease/NoChastity/CockTraining.js
@@ -136,6 +136,7 @@ function startStrokeTraining() {
     }
 
     if (level >= 50) {
+        stopStrokingMessage();
         sendMessage("You're at the highest level...", "You're at the very top", "You reached the highest level!");
         if (getStrictnessForCharacter() == 0) {
             sendMessage("I'm impressed!");
